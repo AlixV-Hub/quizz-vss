2174fc03be9bb614736285aa17bbe089530853f4
Alix VEYRAT <alixveyrat@gmail.com>
2025-02-15 18:09:01 +0100

    second commit

---
+++ questions.js
@@ -0,0 +1,165 @@
+export const QUIZZVSS = {
+    questions: [
+      {
+        text: "Combien de viols ou tentatives de viol ont lieu chaque année en France?",
+        options: [
+          "94 000",
+          "160 000",
+          "210 000",
+          "53 000"
+        ],
+        correct_answer: "210 000"
+      },
+      {
+        text: "Parmi les femmes victimes de viol ou tentatives de viol, combien d’entre elles connaissaient l’agresseur?",
+        options: [
+          "54 %",
+          "74 %",
+          "94 %",
+          "30%"
+        ],
+        correct_answer: "94 %"
+      },
+      {
+        text: "Combien de femmes sont victimes d’agressions sexuelles chaque année en France?",
+        options: [
+          "53 000",
+          "350 000",
+          "94 000",
+          "553 000"
+        ],
+        correct_answer: "553 000"
+      },
+      {
+        text: "Quel pourcentage de viols ou tentatives de viol sont commis par le conjoint ou ex de la victime?",
+        options: [
+          "30%",
+          "43%",
+          "57%",
+          "94%"
+        ],
+        correct_answer: "30%"
+      },
+      {
+        text: "Quel est l'âge avant lequel 77% des victimes de violences sexuelles ont subi les premières violences?",
+        options: [
+          "15 ans",
+          "18 ans",
+          "25 ans",
+          "10 ans"
+        ],
+        correct_answer: "15 ans"
+      },
+      {
+        text: "Les femmes en situation de handicap sont-elles plus exposées aux violences sexuelles que le reste de la population?",
+        options: [
+          "moins exposées",
+          "également exposées",
+          "davantage exposées (2 fois plus)",
+          "pas d'information"
+        ],
+        correct_answer: "davantage exposées (2 fois plus)"
+      },
+      {
+        text: "Quelle proportion des victimes de viol ou tentatives de viol portent plainte?",
+        options: [
+          "moins de 1/10",
+          "2/10",
+          "3/10",
+          "plus de la moitié"
+        ],
+        correct_answer: "moins de 1/10"
+      },
+      {
+        text: "Entre 2007 et 2016, comment ont évolué les plaintes pour viol et les condamnations?",
+        options: [
+          "les plaintes pour viol ont augmenté et les condamnations ont augmenté",
+          "les plaintes pour viol ont diminué et les condamnations ont diminué",
+          "les plaintes pour viol ont augmenté et les condamnations ont diminué",
+          "les plaintes pour viol ont diminué et les condamnations ont augmenté"
+        ],
+        correct_answer: "les plaintes pour viol ont augmenté et les condamnations ont diminué"
+      },
+      {
+        text: "Quelle est la définition exacte du viol selon les sources?",
+        options: [
+          "Tout acte de violence physique.",
+          "Tout acte de pénétration sexuelle, de quelque nature qu’il soit, commis sur la personne d’autrui ou sur la personne de l’auteur par violence.",
+          "Toute forme d'agression sexuelle.",
+          "Acte de harcèlement répété."
+        ],
+        correct_answer: "Tout acte de pénétration sexuelle, de quelque nature qu’il soit, commis sur la personne d’autrui ou sur la personne de l’auteur par violence."
+      },
+      {
+        text: "Qu'est-ce qu'un agissement sexiste?",
+        options: [
+          "Uniquement une violence physique.",
+          "Un acte de pénétration sexuelle.",
+          "Agissement lié au sexe qui porte atteinte à la dignité OU crée un environnement dégradant.",
+          "Une plaisanterie sexuelle isolée."
+        ],
+        correct_answer: "Agissement lié au sexe qui porte atteinte à la dignité OU crée un environnement dégradant."
+      },
+      {
+        text: " Qu'est-ce que le harcèlement sexuel?",
+        options: [
+          "Une simple plaisanterie à connotation sexuelle.",
+          "Des propos ou comportements à connotation sexuelle répétés qui portent atteinte à la dignité OU créent un environnement dégradant.",
+          "Un compliment insistant.",
+          "Une invitation à sortir."
+        ],
+        correct_answer: "Des propos ou comportements à connotation sexuelle répétés qui portent atteinte à la dignité OU créent un environnement dégradant."
+      },
+      {
+        text: "Quelles sont les obligations de l’employeur en matière de violences sexistes et sexuelles?",
+        options: [
+          "Ignorer les faits et laisser les employés gérer seuls.",
+          "Prévenir, faire cesser et sanctionner.",
+          "Mettre en place une médiation entre la victime et l'agresseur.",
+          "Ne rien faire pour ne pas créer de problèmes."
+        ],
+        correct_answer: "Prévenir, faire cesser et sanctionner."
+      },
+      {
+        text: "Qu'est-ce que la culture du viol?",
+        options: [
+          "Uniquement des actes de violence physique.",
+          "Comportements, attitudes et croyances partagés au sein d'une société, comprenant la négation, la minimisation, la banalisation, l'encouragement de la violence, la mise en doute et la culpabilisation.",
+          "Une théorie féministe radicale.",
+          "Un phénomène inexistant."
+        ],
+        correct_answer: "Comportements, attitudes et croyances partagés au sein d'une société, comprenant la négation, la minimisation, la banalisation, l'encouragement de la violence, la mise en doute et la culpabilisation."
+      },
+      {
+        text: "Quelles sont les 5 règles du consentement ?",
+        options: [
+          "Il n'y a pas de règles.",
+          "La victime est toujours consentante.",
+          "Clairement exprimé, libre, éclairé, actif, révocable.",
+          "Le silence vaut consentement."
+        ],
+        correct_answer: "Clairement exprimé, libre, éclairé, actif, révocable."
+      },
+      {
+        text: "Quels sont les différents types de violences sexistes et sexuelles?",
+        options: [
+          "Seules les violences physiques et sexuelles.",
+          "Violences économiques, psychologiques, physiques et sexuelles..",
+          "Violences administratives, économiques, psychologiques, gynécologiques, physiques et sexuelles.",
+          "Violences administratives, physiques et sexuelles."
+        ],
+        correct_answer: "Violences administratives, économiques, psychologiques, gynécologiques, physiques et sexuelles."
+      },
+      // {
+      //   text: "",
+      //   options: [
+      //     "",
+      //     "",
+      //     "",
+      //     ""
+      //   ],
+      //   correct_answer: ""
+      // },
+      // Autres questions...
+    ]
+  };
